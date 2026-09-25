df3567ee8ba72eb97d5342cba1c28c1f87a7a1b1
siyakapdi13-ctrl <siyakapdi13@gmail.com>
2026-09-25 12:12:22 +0530

    Delete extra files

--- backend/app/database/__init__.py
+++
@@ -1,2 +0,0 @@
-from .mongodb import Repository, get_repository, init_repository, close_repository  # noqa: F401
-from .redis import Cache, get_cache, init_cache, close_cache, url_hash  # noqa: F401
--- backend/app/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .url_validation import validate_url, normalize_url, website_name, URLValidationResult  # noqa: F401
-from .responses import success, error  # noqa: F401
